9e0b38f98aa23a1bfa20948d144d7bd602b24b0b
Shahid Yousuf <shahidyousuf77@gmail.com>
2026-01-27 14:57:43 +0530

    Initialize application structure and add basic dependencies

---
+++ src/main/java/com/shahidyousuf/otel_demo/OtelDemoApplication.java
@@ -0,0 +1,13 @@
+package com.shahidyousuf.otel_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtelDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtelDemoApplication.class, args);
+	}
+
+}
